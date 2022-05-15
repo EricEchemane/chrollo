0b9c65338c4ce9c42f20e10cd0a8ff827d6766bc
Eric Echemane <eechemane29@gmail.com>
2022-05-15 15:07:53 +0800

    added test page for testing the package as a devDep

---
+++ pages/test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Row } from 'chrollo-ui';
+
+export default function Test() {
+    return (
+        <Row gap='1rem' p={2} mainAxisAlignment='center'>
+            <div>item</div>
+            <div>item</div>
+            <div>item</div>
+        </Row>
+    );
+}
