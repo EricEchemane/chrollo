cf7ac75a195b748a6ef5950796a08204b993d600
Eric Echemane <eechemane29@gmail.com>
2022-05-15 15:03:43 +0800

    updated build for version 0.1.7

---
+++ dist/components/row.d.ts
@@ -0,0 +1,29 @@
+declare const Row: import("styled-components").StyledComponent<"div", any, import("styled-components").ThemeProps<any> & {
+    p?: string | number | undefined;
+    px?: string | number | undefined;
+    py?: string | number | undefined;
+    pt?: string | number | undefined;
+    pb?: string | number | undefined;
+    pl?: string | number | undefined;
+    pr?: string | number | undefined;
+} & {
+    m?: string | number | undefined;
+    mx?: string | number | undefined;
+    my?: string | number | undefined;
+    mt?: string | number | undefined;
+    mb?: string | number | undefined;
+    ml?: string | number | undefined;
+    mr?: string | number | undefined;
+} & {
+    border?: string | undefined;
+    radius?: string | undefined;
+    shadow?: string | undefined;
+    height?: string | undefined;
+    width?: string | undefined;
+} & {
+    reverse?: boolean | undefined;
+    gap?: string | undefined;
+    mainAxisAlignment?: "center" | "flex-end" | "flex-start" | undefined;
+    crossAxisAlignment?: "center" | "flex-end" | "flex-start" | undefined;
+}, never>;
+export default Row;
